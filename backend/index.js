a8e93a22ef7fe7fcbe8c7ed6a0f090fbe60da9a3
nathanalvares19 <nathanalvares2706@gmail.com>
2026-01-08 12:14:36 +0530

    limit fix

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_APP_PASSWORD,
+    user: process.env.EMAIL_2,
+    pass: process.env.EMAIL_2_PWD,
   },
 });
 
